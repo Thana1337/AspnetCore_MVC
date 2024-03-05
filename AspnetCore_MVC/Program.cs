44b828ceedd2db0e4474060b74f4ddeff501e42f
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-03-05 15:51:28 +0100

    Add project files.

---
+++ AspnetCore_MVC/Program.cs
@@ -0,0 +1,23 @@
+namespace AspnetCore_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
