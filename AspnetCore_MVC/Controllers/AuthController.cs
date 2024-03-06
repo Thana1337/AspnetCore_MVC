5059d20bab80bf85c0f2b960fc87e10332a7d17e
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-03-06 15:03:46 +0100

    Sign up init

--- AspnetCore_MVC/Controllers/AuthController.cs
+++ AspnetCore_MVC/Controllers/AuthController.cs
@@ -4,7 +4,16 @@ namespace AspnetCore_MVC.Controllers
 {
     public class AuthController : Controller
     {
+        [Route("/signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
+        public IActionResult SignIn()
+        {
+            return View();
+        }
 
     }
 }
--- AspnetCore_MVC/Program.cs
+++ AspnetCore_MVC/Program.cs
@@ -1,23 +1,25 @@
-namespace AspnetCore_MVC
+namespace AspnetCore_MVC;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllersWithViews();
-            var app = builder.Build();
-            app.UseHsts();
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
 
-            app.Run();
-        }
+
+
+        var app = builder.Build();
+        app.UseHsts();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
