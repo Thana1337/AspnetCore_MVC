07683031541dea15388800157859a471b884d7ae
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-03-06 14:42:13 +0100

    Sign up start

---
+++ AspnetCore_MVC/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCore_MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
